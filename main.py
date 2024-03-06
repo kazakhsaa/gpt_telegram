4e292eb235ca871114d687b4e643e067f18b9184
MORTYES <75529633+kazakhsaa@users.noreply.github.com>
2024-03-06 20:35:09 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,62 @@
-from pyrogram import Client, filters
+import logging
+import os
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
 import openai
 from config import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
 
 openai.api_key = OPENAI_API_KEY
 
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-@app.on_message(filters.text & ~filters.regex(r'^/'))
-async def handle_message(client, message):
+HEROKU_APP_NAME = os.getenv('yesbol')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+@dp.message_handler(lambda message: not message.text.startswith('/'))
+async def handle_message(message: types.Message):
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": message.text}],
         )
-        await message.reply_text(response.choices[0].message['content'].strip())
+        await message.reply(response.choices[0].message['content'].strip())
     except Exception as e:
-        await message.reply_text(f"Произошла ошибка: {e}")
+        await message.reply(f"Произошла ошибка: {e}")
 
 if __name__ == "__main__":
-    app.run()
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
